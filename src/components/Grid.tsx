35fe8cbd2299933648a8b109a3fbc88da0c67400
Marium <mariumashraf.MA@gmail.com>
2024-07-02 08:28:29 -0400

    commit

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,34 +4,34 @@ import '../styles/Grid.css';
 import Cell from './Cell';
 
 interface GridProps {
-    grid: boolean[][];
-    setGrid: React.Dispatch<React.SetStateAction<boolean[][]>>;
+  grid: boolean[][];
+  setGrid: React.Dispatch<React.SetStateAction<boolean[][]>>;
 }
 
 const Grid: React.FC<GridProps> = ({ grid, setGrid }) => {
-    const toggleCell = (row: number, col: number) => {
-        const newGrid = grid.map((rowArr, rowIndex) =>
-            rowArr.map((cell, colIndex) => {
-                if (rowIndex === row && colIndex === col) {
-                    return !cell;
-                }
-                return cell;
-            })
-        );
-        setGrid(newGrid);
-    };
-
-    return (
-        <div className="grid">
-            {grid.map((row, rowIndex) => (
-                <div key={rowIndex} className="row">
-                {row.map((cell, colIndex) => (
-                    <Cell key={colIndex} isOccupied={cell} onClick={() => toggleCell(rowIndex, colIndex)} />
-                ))}
-                </div>
-        ))}
-        </div>
+  const toggleCell = (row: number, col: number) => {
+    const newGrid = grid.map((rowArr, rowIndex) =>
+      rowArr.map((cell, colIndex) => {
+        if (rowIndex === row && colIndex === col) {
+          return !cell;
+        }
+        return cell;
+      })
     );
+    setGrid(newGrid);
+  };
+
+  return (
+    <div className="grid">
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((cell, colIndex) => (
+            <Cell key={colIndex} isOccupied={cell} onClick={() => toggleCell(rowIndex, colIndex)} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
